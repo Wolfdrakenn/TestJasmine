bb74ed6f30fe07358749a887a020924e2a5cdcd9
Wolfdrakenn <pierre.huvelin@epsi.fr>
2017-12-08 10:49:22 +0100

    je ne sais pas ce que je suis en train de faire

--- src/calc.js
+++ src/calc.js
@@ -7,10 +7,76 @@
 **
 ** La fonction renvoie null en cas d'erreur
 */
-function calculatrice(signe, gauche, droite) {
-    var result = null;
 
-    return result;
+
+function calculatrice(signe, gauche, droite) {
+
+  var result;
+
+    if (typeof gauche === 'number' && typeof droite === 'number') {
+
+	   	if (gauche>=0 && droite<430 && droite!=0) {
+
+			switch (signe) {
+
+				case "*":
+
+					result = gauche*droite;
+
+					return result;
+
+					break;
+
+				case "+":
+
+					result = gauche+droite;
+
+					return result;
+
+					break;
+
+				case "-":
+
+					result = gauche-droite;
+
+					return result;
+
+					break;
+
+				case "/":
+
+					result = gauche/droite;
+
+					return result;
+
+					break;
+
+				default:
+
+					result=null;
+
+					return result;
+
+			} 
+
+		} else {
+
+		   	result = null;
+
+		   	return result;
+
+		}
+
+	}else{
+
+		result=null;
+
+		return result;
+
+	}
+
 }
+
  
+
 module.exports = calculatrice;
